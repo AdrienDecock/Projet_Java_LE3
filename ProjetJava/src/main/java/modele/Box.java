dd0efe321545fba0f52072142353519e7c5b8642
loic mouquet <loic.mouquet59@gmail.com>
2020-11-18 17:58:45 +0100

    creation class

---
+++ ProjetJava/src/main/java/modele/Box.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author loic
+ */
+public class Box {
+    
+    private String idBox;
+    
+    private int hBox;
+    
+    private int lBox;
+    
+    private float prixBox;
+    
+    
+}
---
+++ ProjetJava/src/main/java/modele/Produit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author loic
+ */
+public class Produit {
+    
+    private String idProd;
+    
+    private int hProd;
+    
+    private int lProd;
+    
+    private int quantite;
+    
+    
+}
