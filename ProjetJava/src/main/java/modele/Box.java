2ba9a9f029e40ddea70c8bcc678e9b1547d09c72
NadaEl <59821934+NadaEl@users.noreply.github.com>
2020-12-02 18:28:10 +0100

    nada_021220
    Ajout constructeurs et getters aux 3 classes (box - produit - instance)

--- ProjetJava/src/main/java/modele/Box.java
+++ ProjetJava/src/main/java/modele/Box.java
@@ -10,14 +10,39 @@ package modele;
  * @author loic
  */
 public class Box {
-    
+        //4 attributs
+    // l'identifiant du Box
     private String idBox;
-    
+    //la hauteur de la Box
     private int hBox;
-    
+    //la longueur de la Box
     private int lBox;
-    
+    //le prix de la Box
     private float prixBox;
     
+        //constructeur
+    public Box(String idBox, int hBox, int lBox, float prixBox) {
+        this.idBox = idBox;
+        this.hBox = hBox;
+        this.lBox = lBox;
+        this.prixBox = prixBox;
+    }
     
+        //getters
+    //Recupere l'identifiant de la Box
+    public String getIdentifiant() {
+        return idBox;
+    }
+    //Recupere la longueur de la Box
+    public int getLongueur() {
+        return lBox;
+    }
+    //Recupere la hauteur de la Box
+    public int getHauteur() {
+        return hBox;
+    }
+    //Recupere le prix de la Box
+    public float getPrix() {
+        return prixBox;
+    }
 }
--- ProjetJava/src/main/java/modele/Instance.java
+++ ProjetJava/src/main/java/modele/Instance.java
@@ -10,12 +10,27 @@ package modele;
  * @author loic
  */
 public class Instance {
-    
+        //2 attributs
+    //l'identifiant de l'instance
     private String idInstance;
-    
+    //le nom de l'instance
     private String nom;
     
+        //constructeur par données
+    public Instance(String idInstance, String nom) {
+        this.idInstance = idInstance;
+        this.nom = nom;
+    }
     
+        //getters
+    //permet de recuperer l'identifiant de l'instance
+    public String getIdInstance() {
+        return idInstance;
+    }
+    //permet de recuperer le nom de l'instance
+    public String getNom() {
+        return nom;
+    }
     
     
 }
